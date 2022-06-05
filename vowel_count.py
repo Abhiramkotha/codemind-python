0cf0d439cc43623266c7dfbfc47ef755e2c2e5c8
Abhiramkotha <97886970+Abhiramkotha@users.noreply.github.com>
2022-06-05 22:06:58 +0530

    s=input() c=0 for char in s:     if char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U':         c+=1 print(c)

---
+++ vowel_count.py
@@ -0,0 +1,6 @@
+s=input()
+c=0
+for char in s:
+    if char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U':
+        c+=1
+print(c)
